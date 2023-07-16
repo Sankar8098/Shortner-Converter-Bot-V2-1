22195ba615bffd66a3bcfa1a8586aa59bb9aed05
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-07-16 09:54:14 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -18,7 +18,7 @@ async def get_user(user_id):
             "header_text": "",
             "footer_text": "",
             "username": None,
-            "base_site": "syshort.com",
+            "base_site": "dalink.in",
             "banner_image": None,
             "is_banner_image": True,
             "is_username": True,
